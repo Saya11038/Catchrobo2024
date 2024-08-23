f502c85bb4e9a431cfccd9b42eae51ffe0595435
saya_38 <saya.koko.nana.1013@gmail.com>
2024-08-24 00:19:27 +0900

    Dynamixel速度制御動確済

---
+++ ROS2/build/dynamixel_sdk_examples/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/saya/Catchrobo2024/ROS2/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
+. "/home/saya/Catchrobo2024/ROS2/install/dynamixel_sdk_custom_interfaces/share/dynamixel_sdk_custom_interfaces/package.sh"
